b9c82bc115339a883ee206dc7192e6dcdce4a9f6
Venky <venkatramanm@gmail.com>
2025-06-27 17:03:17 +0530

    Extends payent module's Entity

--- src/main/java/com/venky/swf/plugins/razorpay/db/model/buyers/Company.java
+++ src/main/java/com/venky/swf/plugins/razorpay/db/model/buyers/Company.java
@@ -1,4 +1,4 @@
 package com.venky.swf.plugins.razorpay.db.model.buyers;
 
-public interface Company extends com.venky.swf.plugins.collab.db.model.participants.admin.Company , Buyer {
+public interface Company extends com.venky.swf.plugins.payments.db.model.payment.Company, Buyer {
 }
